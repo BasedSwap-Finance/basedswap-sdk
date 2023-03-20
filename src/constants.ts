a41a8daba8090bdfb0a6e53fd583d1d86820a291
BasedSwapDev <126929489+BasedSwapDev@users.noreply.github.com>
2023-03-20 00:48:08 -0700

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x5b8416c8678C0d859e7A79ef5b9583a7f8D3Bf63'
+
+export const INIT_CODE_HASH = '0xaa29ab4eb16e680e4a1ec9a05a9181bc311b55e0f9732fc8adc6f54752942cff'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
