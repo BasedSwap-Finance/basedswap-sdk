58db252941c6f8e9f1df9eae8cc358b7802d88fb
RevDev <86997551+RevDevPat@users.noreply.github.com>
2023-03-20 02:36:03 -0700

    f

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x5b8416c8678C0d859e7A79ef5b9583a7f8D3Bf63";
+export declare const INIT_CODE_HASH = "0xaa29ab4eb16e680e4a1ec9a05a9181bc311b55e0f9732fc8adc6f54752942cff";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
