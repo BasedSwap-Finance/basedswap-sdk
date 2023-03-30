15e13ad74a6352cd441aee4dc494e65d37b3c3d6
MarcoMandar <malicemmandar@gmail.com>
2023-03-31 00:38:38 +0300

    update

--- dist/constants.d.ts
+++ dist/constants.d.ts
@@ -1,6 +1,6 @@
 import JSBI from 'jsbi';
-export declare const FACTORY_ADDRESS = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-export declare const INIT_CODE_HASH = "0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f";
+export declare const FACTORY_ADDRESS = "0x3F184EcCeb14270ae8Da51e0D94ce9C328A26859";
+export declare const INIT_CODE_HASH = "0x97fe61384d0aed6f145eff143d6773691f987c4147f3a1503ed6305a96d87a56";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
 export declare const ONE: JSBI;
